2f3d22cc8701f83c2ce10885af699fbba4dcd76d
aEspada-tv <77913024+aEspada-tv@users.noreply.github.com>
2021-01-25 03:59:20 -0300

    Add files via upload

---
+++ playerMovement_EASY.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement_EASY : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 4f;
+    [SerializeField]
+    private float _jumpForce = 8f;
+
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    private void Update()
+    {
+        var movement = Input.GetAxis("Horizontal");
+        transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * _speed;
+
+        if (!Mathf.Approximately(0, movement))
+        {
+            transform.rotation = movement < 0 ? Quaternion.Euler(0, 180, 0) : Quaternion.identity;
+        }
+
+        if (Input.GetButtonDown("Jump") && Mathf.Abs(rb.velocity.y) < 0.0001f)
+        {
+            rb.AddForce(new Vector2(0, _jumpForce), ForceMode2D.Impulse);
+        }
+    }
+
+    
+}
---
+++ playerMovement_EASY2.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement_EASY2 : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed;
+    [SerializeField]
+    private float _jumpForce;
+
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    private void Update()
+    {
+        var horizontalMovement = Input.GetAxis("Horizontal");
+        transform.position += new Vector3(horizontalMovement, 0, 0) * Time.deltaTime * _speed;
+
+        if (!Mathf.Approximately(0, horizontalMovement))
+        {
+            transform.rotation = horizontalMovement < 0 ? Quaternion.Euler(0, 180, 0) : Quaternion.identity;
+        }
+
+        var verticalMovement = Input.GetAxis("Vertical");
+        transform.position += new Vector3(0, verticalMovement, 0) * Time.deltaTime * _speed;
+
+    }
+
+    
+}
